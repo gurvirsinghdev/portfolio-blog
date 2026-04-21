dc750c6337892b03d0606137f06ac23411647dc4
Gurvir Singh <gurvirsingh.dev@gmail.com>
2026-04-21 16:30:25 -0700

    made the landing page more responsive

--- src/lib/scaling.ts
+++ src/lib/scaling.ts
@@ -1,7 +1,14 @@
-export const scaling = {
+export const desktop = {
   converse: 4 / 3,
   inverse: 3 / 4
 } as const;
+
+export const mobile = {
+  converse: 1,
+  inverse: 1
+} as const;
+
+export const scaling = window.innerWidth > 1280 ? desktop : mobile;
 
 export const makeCSSCalc = (cssWidth: string, scaling_type: typeof scaling[keyof typeof scaling]) => {
   return `calc(${cssWidth}*${scaling_type})`;
