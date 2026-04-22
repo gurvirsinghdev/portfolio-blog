e73f3b44e66a4f693ce825ea331a39f8abc20c7d
Gurvir Singh <gurvirsingh.dev@gmail.com>
2026-04-21 20:01:54 -0700

    loading posts from the db

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,25 @@
+import { db } from '#/lib/db';
 import { makeCSSCalc, makeScalingFactor, mobileScaling } from '#/lib/scaling'
+import { postsTable } from '#/schema';
 import { createFileRoute, Link } from '@tanstack/react-router'
+import { createServerFn } from '@tanstack/react-start';
+import { desc } from 'drizzle-orm';
+
+const getPosts = createServerFn({ method: "GET" })
+  .handler(() => db.select().from(postsTable).orderBy(desc(postsTable.createdAt)));
 
 export const Route = createFileRoute('/')({
   component: Home,
+  loader: () => getPosts()
 })
 
 function Home() {
   const scaling = window.innerWidth > 1280 ? makeScalingFactor(3.5 / 3) : mobileScaling;
+  const posts = Route.useLoaderData().map((post) => ({
+    ...post,
+    description: (post.description.trim().split("\n")[0] ?? "").trim()
+  }));
+
   return (
     <main className='w-full h-full' style={{ scale: scaling.converse, transformOrigin: 'top left', maxWidth: makeCSSCalc('100%', scaling.inverse) }}>
       <section className='max-w-[min(calc(100%-4rem),2048px)] w-full h-full mx-auto'>
@@ -16,21 +29,19 @@ function Home() {
         </section>
 
         <section className='grid gap-12 md:gap-10 lg:gap-8 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 auto-rows-fr'>
-          {[1, 2, 3, 4, 5, 6, 7].map((post, idx) => (
-            <Link key={idx} to={'/$postId'} params={{ postId: post.toString() }}>
+          {posts.map((post, idx) => (
+            <Link key={idx} to={'/$postId'} params={{ postId: post.id }}>
               <div className='space-y-3 group border-2'>
                 <div className='bg-muted w-full' style={{
                   minHeight: makeCSSCalc('328px', scaling.inverse),
                   height: makeCSSCalc('328px', scaling.inverse),
                   maxHeight: makeCSSCalc('328px', scaling.inverse)
                 }}>
-                  <img className='w-full h-full object-cover' src={`https://picsum.photos/1280/720?${idx}`} alt='Blog post cover' />
+                  <img className='w-full h-full object-cover' src={`https://picsum.photos/seed/${post.title.replaceAll(' ', '')}/1280/720?${idx}`} alt='Blog post cover' />
                 </div>
                 <div className='space-y-1 p-3'>
-                  <h2 className='text-xl group-hover:underline font-semibold text-foreground leading-snug'>How Remote Work Is Changing Small Town Economies</h2>
-                  <p className='text-surface'>As more people leave big cities for quieter communities, small towns are
-                    seeing unexpected growth. Here’s how remote work is reshaping local
-                    businesses, housing, and daily life.</p>
+                  <h2 className='text-xl capitalize group-hover:underline line-clamp-2 font-semibold text-foreground leading-snug'>{post.title}</h2>
+                  <p className='text-surface line-clamp-3'>{post.description}</p>
                 </div>
               </div>
 
--- src/schema/index.ts
+++ src/schema/index.ts
@@ -6,3 +6,12 @@ export const usersTable = p.pgTable("users", {
   email: p.text().notNull().unique(),
   password: p.text().notNull(),
 });
+
+export const postsTable = p.pgTable("posts", {
+  id: p.uuid().primaryKey().defaultRandom(),
+  user_id: p.uuid().notNull().references(() => usersTable.id),
+  title: p.text().notNull().unique(),
+  description: p.text().notNull(),
+  createdAt: p.timestamp().defaultNow(),
+  updatedAt: p.timestamp().defaultNow()
+});
